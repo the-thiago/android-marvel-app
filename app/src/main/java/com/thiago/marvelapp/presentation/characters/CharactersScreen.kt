6911c9eacb12ad8d1927746066dd68c9361b82a5
Thiago Moreira de Souza <thiago.moreira.souza@gmail.com>
2022-06-20 19:48:47 -0300

    Data interfaces and Character screen

--- app/src/main/java/com/thiago/marvelapp/presentation/characters/CharactersScreen.kt
+++ app/src/main/java/com/thiago/marvelapp/presentation/characters/CharactersScreen.kt
@@ -1,15 +1,22 @@
 package com.thiago.marvelapp.presentation.characters
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.thiago.core.domain.model.Character
 import com.thiago.marvelapp.R
 import com.thiago.marvelapp.ui.theme.MarvelAppTheme
 
@@ -23,9 +30,38 @@ internal fun CharactersScreen() {
         },
         modifier = Modifier.fillMaxSize()
     ) {
-        Column(modifier = Modifier.padding(it)) {
-            Text(text = "CharactersScreen")
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+
         }
+    }
+}
+
+@Composable
+private fun Character(character: Character) {
+    Column(
+        modifier = Modifier
+            .height(230.dp)
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(character.imageUrl)
+                .crossfade(true)
+                .build(),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(170.dp)
+        )
+        Text(
+            text = character.name,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+        )
     }
 }
 
---
+++ core/src/main/java/com/thiago/core/data/repository/CharactersRepository.kt
@@ -0,0 +1,23 @@
+package com.thiago.core.data.repository
+
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import com.thiago.core.domain.model.Comic
+import com.thiago.core.domain.model.Event
+import com.thiago.core.domain.model.Character
+import kotlinx.coroutines.flow.Flow
+
+interface CharactersRepository {
+
+    fun getCharacters(query: String): PagingSource<Int, Character>
+
+    fun getCachedCharacters(
+        query: String,
+        pagingConfig: PagingConfig
+    ): Flow<PagingData<Character>>
+
+    suspend fun getComics(characterId: Int): List<Comic>
+
+    suspend fun getEvents(characterId: Int): List<Event>
+}
